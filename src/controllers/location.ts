5d980340a6c2a07369a2d4c95ecf9cdee5deff07
bravint <bravint@gmail.com>
2022-04-01 12:32:19 +0100

    add location controllers

--- src/controllers/location.ts
+++ src/controllers/location.ts
@@ -1,5 +1,39 @@
 import { Request, Response } from 'express';
 
-export const createLocation = async (req: Request, res: Response) => {};
+import { prisma } from '../utils/prisma';
 
-export const getLocationsByUser = async (req: Request, res: Response) => {};
+export const createLocation = async (req: Request, res: Response) => {
+    const { name, longitude, latitude } = req.body;
+
+    const createdLocation = await prisma.location.create({
+        data: {
+            name: name,
+            longitude: longitude,
+            latitude: latitude,
+        },
+    });
+
+    res.status(200).json({ data: createdLocation });
+};
+
+export const getLocationsByUser = async (req: Request, res: Response) => {
+    const { id } = req.body;
+
+    const selectedLocations = await prisma.location.findMany({
+        where: {
+            users: {
+                some: {
+                    user: {
+                        id: Number(id),
+                    },
+                },
+            },
+        },
+        include: {
+            ratings: true,
+        },
+    });
+
+    res.status(200).json({ data: selectedLocations });
+};
+
