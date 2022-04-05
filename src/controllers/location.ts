093c863a3edab18acaa24fbb9bf52558a88d9e5d
bravint <bravint@gmail.com>
2022-04-05 14:16:18 +0100

    fix location edit, delete controllers

--- src/controllers/location.ts
+++ src/controllers/location.ts
@@ -26,7 +26,7 @@ export const createLocation = async (req: Request, res: Response) => {
     });
 
     if (!createdLocation) {
-        return res.sendStatus(500)
+        return res.sendStatus(500);
     }
 
     res.status(200).json({ data: createdLocation });
@@ -45,4 +45,56 @@ export const getLocationsByUser = async (req: Request, res: Response) => {
     });
 
     res.status(200).json({ data: selectedLocations });
+};
+
+export const editLocation = async (req: Request, res: Response) => {
+    const { user }: any = req;
+
+    const { id }: any = req.params;
+
+    const { name, lng, lat, rating, ratingId } = req.body;
+
+    console.log(name, lng, lat, rating);
+
+    const editedLocation = await prisma.location.update({
+        where: {
+            id: Number(id),
+        },
+        data: {
+            name: name,
+            lng: lng,
+            lat: lat,
+            userId: Number(user.id),
+            rating: {
+                update: {
+                    where: {
+                        id: Number(ratingId),
+                    },
+                    data: {
+                        ratings: rating,
+                        userId: Number(user.id),
+                    },
+                },
+            },
+        },
+        include: {
+            rating: true,
+        },
+    });
+
+    console.log('editedLocation', editedLocation);
+
+    res.status(201).json({ data: editedLocation });
+};
+
+export const deleteLocation = async (req: Request, res: Response) => {
+    const { id }: any = req.params;
+
+    const deletedLocation = await prisma.location.delete({
+        where: {
+            id: Number(id),
+        },
+    });
+
+    res.status(201).json({ data: deletedLocation });
 };
--- src/routes/location.ts
+++ src/routes/location.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 
-import { createLocation, getLocationsByUser } from '../controllers/location';
+import { createLocation, getLocationsByUser, editLocation, deleteLocation } from '../controllers/location';
 import { callPassportJwt } from '../utils/passport/middleware/jwt';
 
 import '../utils/passport/strategy/jwt';
@@ -10,5 +10,9 @@ const router = Router();
 router.post('/', callPassportJwt, createLocation);
 
 router.get('/', callPassportJwt, getLocationsByUser);
+
+router.post('/:id', callPassportJwt, editLocation);
+
+router.delete('/:id', callPassportJwt, deleteLocation);
 
 export default router;
