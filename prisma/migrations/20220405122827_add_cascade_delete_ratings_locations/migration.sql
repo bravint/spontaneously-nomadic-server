8096022e12d0850df013c97fe50e3ff61f8a099f
bravint <bravint@gmail.com>
2022-04-06 11:50:38 +0100

    re-init prisma

--- prisma/migrations/20220405122827_add_cascade_delete_ratings_locations/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Rating" DROP CONSTRAINT "Rating_locationId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Rating" ADD CONSTRAINT "Rating_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20220406104752_init/migration.sql
+++ prisma/migrations/20220406104752_init/migration.sql
@@ -2,13 +2,12 @@
 CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 
 -- CreateEnum
-CREATE TYPE "Provider" AS ENUM ('NONE', 'GOOGLE', 'FACEBOOK', 'APPLE');
+CREATE TYPE "Provider" AS ENUM ('NONE', 'GOOGLE', 'FACEBOOK');
 
 -- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
-    "email" TEXT,
-    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT,
     "oAuthId" TEXT,
     "provider" "Provider" NOT NULL DEFAULT E'NONE',
@@ -23,6 +22,7 @@ CREATE TABLE "User" (
 CREATE TABLE "Profile" (
     "id" SERIAL NOT NULL,
     "userId" INTEGER NOT NULL,
+    "username" TEXT,
     "profileImage" TEXT,
     "bio" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -34,10 +34,11 @@ CREATE TABLE "Profile" (
 -- CreateTable
 CREATE TABLE "Rating" (
     "id" SERIAL NOT NULL,
-    "rating" INTEGER NOT NULL,
+    "ratings" INTEGER NOT NULL,
     "userId" INTEGER NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "locationId" INTEGER NOT NULL,
 
     CONSTRAINT "Rating_pkey" PRIMARY KEY ("id")
 );
@@ -46,40 +47,32 @@ CREATE TABLE "Rating" (
 CREATE TABLE "Location" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "longitude" TEXT NOT NULL,
-    "latitude" TEXT NOT NULL,
+    "lng" DOUBLE PRECISION NOT NULL,
+    "lat" DOUBLE PRECISION NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" INTEGER,
 
     CONSTRAINT "Location_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "RatingsOnLocations" (
-    "id" SERIAL NOT NULL,
-    "locationId" INTEGER NOT NULL,
-    "ratingId" INTEGER NOT NULL,
-
-    CONSTRAINT "RatingsOnLocations_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "locationsOnUsers" (
-    "id" SERIAL NOT NULL,
-    "locationId" INTEGER NOT NULL,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "locationsOnUsers_pkey" PRIMARY KEY ("id")
+CREATE TABLE "_Follows" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_oAuthId_key" ON "User"("oAuthId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_oAuthId_key" ON "User"("oAuthId");
+CREATE UNIQUE INDEX "_Follows_AB_unique" ON "_Follows"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_Follows_B_index" ON "_Follows"("B");
 
 -- AddForeignKey
 ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -88,13 +81,13 @@ ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId"
 ALTER TABLE "Rating" ADD CONSTRAINT "Rating_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RatingsOnLocations" ADD CONSTRAINT "RatingsOnLocations_ratingId_fkey" FOREIGN KEY ("ratingId") REFERENCES "Rating"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Rating" ADD CONSTRAINT "Rating_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "RatingsOnLocations" ADD CONSTRAINT "RatingsOnLocations_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Location" ADD CONSTRAINT "Location_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "locationsOnUsers" ADD CONSTRAINT "locationsOnUsers_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "_Follows" ADD FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "locationsOnUsers" ADD CONSTRAINT "locationsOnUsers_locationId_fkey" FOREIGN KEY ("locationId") REFERENCES "Location"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "_Follows" ADD FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
